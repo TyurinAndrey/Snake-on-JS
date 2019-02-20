966b75cf1708fb82d45fd5a273c19fa7b04b69f7
Tyurin Andrey <44750239+TyurinAndrey@users.noreply.github.com>
2019-02-20 21:35:53 +0300

    Done

--- script.js
+++ script.js
@@ -50,3 +50,133 @@ snakeBody[0].classList.add("head");
 
 //создание мыши
 
+let mouse;
+
+function createMouse() {
+    function generateMouse() {
+        let posX = Math.round(Math.random() * (10-3) + 3);
+        let posY = Math.round(Math.random() * (10-1) + 1);
+        return[posX, posY];
+    }
+    let mouseCoordinates = generateMouse();
+    console.log(mouseCoordinates);
+    mouse = document.querySelector('[posX = "' + mouseCoordinates[0] + '"][posY = "' + 
+    mouseCoordinates[1] + '"]');
+
+    while(mouse.classList.contains("snakeBody")) {
+        let mouseCoordinates = generateMouse();
+        mouse = document.querySelector('[posX = "' + mouseCoordinates[0] + '"][posY = "' + 
+        mouseCoordinates[1] + '"]');
+    }
+
+    mouse.classList.add("mouse");
+}
+
+createMouse();
+
+//задание движения для мыши
+
+let direction = "right";
+let steps = false;
+
+let input = document.createElement("input");
+document.body.appendChild(input);
+
+
+let score = 0;
+input.value = "Ваши очки: " + score;
+
+function move() {
+    let snakeCoordinates = [snakeBody[0].getAttribute("posX"), snakeBody[0].getAttribute("posY")];
+    snakeBody[0].classList.remove("head");
+    snakeBody[snakeBody.length-1].classList.remove("snakeBody");
+    snakeBody.pop();
+
+    if (direction == "right"){
+        if (snakeCoordinates[0] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' +(+snakeCoordinates[0]+1) + '"][posY = "' + 
+            snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "1"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    
+    } else if (direction == "left"){
+        if (snakeCoordinates[0] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' +(+snakeCoordinates[0]-1) + '"][posY = "' + 
+            snakeCoordinates[1] + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "10"][posY = "' + snakeCoordinates[1] + '"]'));
+        }
+    
+    } else if (direction == "up"){
+        if (snakeCoordinates[1] < 10) {
+            snakeBody.unshift(document.querySelector('[posX = "' +snakeCoordinates[0] + '"][posY = "' + 
+            (+snakeCoordinates[1]+1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' +snakeCoordinates[0] + '"][posY = "1"]'));
+        }
+    
+    } else if (direction == "down"){
+        if (snakeCoordinates[1] > 1) {
+            snakeBody.unshift(document.querySelector('[posX = "' +snakeCoordinates[0] + '"][posY = "' + 
+            (snakeCoordinates[1]-1) + '"]'));
+        } else {
+            snakeBody.unshift(document.querySelector('[posX = "' +snakeCoordinates[0] + '"][posY = "10"]'));
+        }
+    
+    } 
+    
+    if (snakeBody[0].getAttribute("posX") == mouse.getAttribute("posX") && snakeBody[0].getAttribute("posY") == mouse.getAttribute("posY")) {
+        mouse.classList.remove("mouse");
+        let a = snakeBody[snakeBody.length-1].getAttribute("posX");
+        let b = snakeBody[snakeBody.length-1].getAttribute("posY");
+        snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
+        createMouse();
+        score++;
+        input.value = "Ваши очки: " + score;
+    }
+    
+    if (snakeBody[0].classList.contains("snakeBody")) {
+        setTimeout(() => {
+            alert("Игра окончена. Ваши очки: " + score);
+        }, 200)
+        
+        clearInterval(interval);
+        snakeBody[0].style.background = "url(scream.jpg) center no-repeat";
+        snakeBody[0].style.backgroundSize = "cover";
+    }
+
+    snakeBody[0].classList.add("head");
+    for (let i = 0; i < snakeBody.length; i++) {
+        snakeBody[i].classList.add("snakeBody");
+    }
+
+    steps = true;
+}
+
+let interval = setInterval(move, 300);
+
+//проверка текущего направления и задание нового
+window.addEventListener("keydown", function(e) {
+    if (steps == true) {
+        if (e.keyCode == 37 && direction!= "right") {
+            direction = "left";
+            steps = false;
+        }
+        else if (e.keyCode == 38 && direction!= "down") {
+            direction = "up";
+            steps = false;
+        }
+        
+        else if (e.keyCode == 39 && direction!= "left") {
+            direction = "right";
+            steps = false;
+        }
+        
+        else if (e.keyCode == 40 && direction!= "up") {
+            direction = "down";
+            steps = false;
+        }
+    }
+    
+});
